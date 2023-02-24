3b04a27a704ef00f7cb21469f29b65437a850efb
Dennis Kamau <devjam.ke@gmail.com>
2023-02-24 17:02:48 +0300

    Create a Log Out link and set the destroy session action and path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#dashboard'
+  get '/users/sign_out', to: 'sessions#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
